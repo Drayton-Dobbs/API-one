d86bfad61c72be2d5cd4af0be598d8dffcf9fec6
Drayton-Dobbs <jamesdraytondobbs@gmail.com>
2018-10-12 16:57:47 -0500

    so happy

---
+++ src/injectables/utils.js
@@ -0,0 +1,15 @@
+class Utils {
+    capitalize(str) {
+        return str.slice(0,1).toUpperCase() + str.slice(1);
+    }
+
+    toCamelCase() {
+
+    }
+
+    prettyPhoneNumber() {
+
+    }
+}
+
+export default Utils;
